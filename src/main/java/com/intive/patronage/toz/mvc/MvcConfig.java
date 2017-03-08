6cc42114695e9ffd9c705e74c134823b52d820fe
Jakub <jakub.izbicki1@gmail.com>
2017-03-08 20:55:20 +0100

    implements swagger into project

---
+++ src/main/java/com/intive/patronage/toz/mvc/MvcConfig.java
@@ -0,0 +1,45 @@
+package com.intive.patronage.toz.mvc;
+
+import org.springframework.context.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.servlet.config.annotation.*;
+import springfox.documentation.builders.*;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.time.LocalDate;
+
+import static springfox.documentation.builders.PathSelectors.regex;
+
+@EnableWebMvc
+@Configuration
+@EnableSwagger2
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(regex("/api/.+"))
+                .build()
+                .apiInfo(apiInfo())
+                .directModelSubstitute(LocalDate.class, String.class)
+                .genericModelSubstitutes(ResponseEntity.class);
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Patronage 2017 TOZ-BE")
+                .description("REST API made in Spring Boot")
+                .version("1").build();
+    }
+}
