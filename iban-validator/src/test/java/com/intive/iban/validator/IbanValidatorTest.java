9144163884d38d4da4e6c9ec3e57b1450f918bbb
Jakub <jakub.izbicki1@gmail.com>
2017-03-10 15:04:55 +0100

    implements parameterized tests using DataProvider

--- iban-validator/src/test/java/com/intive/iban/validator/IbanValidatorTest.java
+++ iban-validator/src/test/java/com/intive/iban/validator/IbanValidatorTest.java
@@ -1,12 +1,19 @@
 package com.intive.iban.validator;
 
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
-import javax.validation.*;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
 import java.util.Set;
 
+@RunWith(DataProviderRunner.class)
 public class IbanValidatorTest {
 
     private Validator validator;
@@ -19,61 +26,31 @@ public class IbanValidatorTest {
     }
 
     @Test
-    public void testValidIban() {
-        SampleIban iban1 = new SampleIban("AL47 2121 1009 0000 0002 3569 8741");
-        SampleIban iban2 = new SampleIban("AT611904300234573201");
-        SampleIban iban3 = new SampleIban("DE89 3704 0044 0532 0130 00");
-        SampleIban iban4 = new SampleIban("PL60 1020 1026 0000 0422 7020 1111");
-
-        Assert.assertTrue(iban1.isIbanValid());
-        Assert.assertTrue(iban2.isIbanValid());
-        Assert.assertTrue(iban3.isIbanValid());
-        Assert.assertTrue(iban4.isIbanValid());
+    @UseDataProvider(value = "dataProviderValidIban", location = DataProviderIbanTest.class)
+    public void testValidIban(String ibanData) {
+        SampleIban iban = new SampleIban(ibanData);
+        Assert.assertTrue(iban.isIbanValid());
     }
 
     @Test
-    public void testInvalidIbanBadCountryCodeOrLength() {
-        SampleIban iban1 = new SampleIban("XX47 2121 1009 0000 0002 3569 8741");
-        SampleIban iban2 = new SampleIban("AT6011904300234573201");
-        SampleIban iban3 = new SampleIban("DE89 3704 0044 0532 0130 0");
-        SampleIban iban4 = new SampleIban("2260 1020 1026 0000 0422 7020 1111");
-
-        Assert.assertFalse(iban1.isIbanValid());
-        Assert.assertFalse(iban2.isIbanValid());
-        Assert.assertFalse(iban3.isIbanValid());
-        Assert.assertFalse(iban4.isIbanValid());
+    @UseDataProvider(value = "dataProviderInvalidIbanBadCountryCodeOrLength", location = DataProviderIbanTest.class)
+    public void testInvalidIbanBadCountryCodeOrLength(String ibanData) {
+        SampleIban iban = new SampleIban(ibanData);
+        Assert.assertFalse(iban.isIbanValid());
     }
 
     @Test
-    public void testInvalidIbanBadFormat() {
-        SampleIban iban1 = new SampleIban("AL47 2121 1009 00 00 0002 3569 8741");
-        SampleIban iban2 = new SampleIban(" AT611904300234573201");
-        SampleIban iban3 = new SampleIban("DE89 3704 0044 05320130 00");
-        SampleIban iban4 = new SampleIban("PL60 1020 1026 0000 0422 7020 111 1");
-        SampleIban iban5 = new SampleIban(null);
-        SampleIban iban6 = new SampleIban("");
-        SampleIban iban7 = new SampleIban(" ");
-
-        Assert.assertFalse(iban1.isIbanValid());
-        Assert.assertFalse(iban2.isIbanValid());
-        Assert.assertFalse(iban3.isIbanValid());
-        Assert.assertFalse(iban4.isIbanValid());
-        Assert.assertFalse(iban5.isIbanValid());
-        Assert.assertFalse(iban6.isIbanValid());
-        Assert.assertFalse(iban7.isIbanValid());
+    @UseDataProvider(value = "dataProviderInvalidIbanBadFormat", location = DataProviderIbanTest.class)
+    public void testInvalidIbanBadFormat(String ibanData) {
+        SampleIban iban = new SampleIban(ibanData);
+        Assert.assertFalse(iban.isIbanValid());
     }
 
     @Test
-    public void testInvalidIbanUnallowedCharacters() {
-        SampleIban iban1 = new SampleIban("AL47 \n21 1009 0000 0002 3569 8741");
-        SampleIban iban2 = new SampleIban("AT61.904300234573201");
-        SampleIban iban3 = new SampleIban("DE89-3704-0044-0532-0130-00");
-        SampleIban iban4 = new SampleIban("PL60 \\n+ 1026 0000 0422 7020 1111");
-
-        Assert.assertFalse(iban1.isIbanValid());
-        Assert.assertFalse(iban2.isIbanValid());
-        Assert.assertFalse(iban3.isIbanValid());
-        Assert.assertFalse(iban4.isIbanValid());
+    @UseDataProvider(value = "dataProviderInvalidIbanUnallowedCharacters", location = DataProviderIbanTest.class)
+    public void testInvalidIbanUnallowedCharacters(String ibanData) {
+        SampleIban iban = new SampleIban(ibanData);
+        Assert.assertFalse(iban.isIbanValid());
     }
 
     public class SampleIban {
@@ -81,11 +58,11 @@ public class IbanValidatorTest {
         @IBAN
         String iban;
 
-        public SampleIban(String iban) {
+        SampleIban(String iban) {
             this.iban = iban;
         }
 
-        public boolean isIbanValid() {
+        boolean isIbanValid() {
             Set<ConstraintViolation<SampleIban>> violations = validator.validate(this);
             return !(violations.size() > 0);
         }
