6071a421aba83f341b1057a78cbd57676d34ec9e
Jakub <jakub.izbicki1@gmail.com>
2017-03-10 14:36:39 +0100

    adds COUNTRY_CODE in config

--- iban-validator/src/main/java/com/intive/iban/validator/IbanValidator.java
+++ iban-validator/src/main/java/com/intive/iban/validator/IbanValidator.java
@@ -4,21 +4,42 @@ import org.iban4j.*;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
+import java.io.*;
+import java.util.Properties;
 
 public class IbanValidator implements ConstraintValidator<IBAN, String> {
 
+    private String COUNTRY_CODE;
+    private final String DEFAULT_COUNTRY_CODE = "PL";
+
     public void initialize(IBAN constraintAnnotation) {
+        getCountryCodeFromConfig();
     }
 
     public boolean isValid(String iban, ConstraintValidatorContext context) {
         try {
-            IbanUtil.validate(iban);
+            IbanUtil.validate(COUNTRY_CODE + iban);
             return true;
         } catch (IbanFormatException | InvalidCheckDigitException | UnsupportedCountryException e) {
-            String errorMessage = String.format("{com.intive.iban.validator.IbanValidator.%s}", e.getMessage());
-            context.disableDefaultConstraintViolation();
-            context.buildConstraintViolationWithTemplate(errorMessage).addConstraintViolation();
+            setErrorMessage(context, e.getMessage());
             return false;
         }
+    }
+
+    private void getCountryCodeFromConfig() {
+        try {
+            Properties prop = new Properties();
+            InputStream input = new FileInputStream("src\\main\\resources\\application.properties");
+            prop.load(input);
+            COUNTRY_CODE = prop.getProperty("COUNTRY_CODE");
+        } catch (IOException e) {
+            COUNTRY_CODE = DEFAULT_COUNTRY_CODE;
+        }
+    }
+
+    private void setErrorMessage(ConstraintValidatorContext context, String message) {
+        String errorMessage = String.format("{com.intive.iban.validator.IbanValidator.%s}", message);
+        context.disableDefaultConstraintViolation();
+        context.buildConstraintViolationWithTemplate(errorMessage).addConstraintViolation();
     }
 }
