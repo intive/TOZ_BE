6d2554366499729a4c92711675ce2c7809dcd18c
Jakub <jakub.izbicki1@gmail.com>
2017-03-15 23:32:24 +0100

    implements custom @ExceptionHandler instead of handleMethodArgumentNotValid() to keep consistency in file

--- src/main/java/com/intive/patronage/toz/controller/ControllerExceptionHandler.java
+++ src/main/java/com/intive/patronage/toz/controller/ControllerExceptionHandler.java
@@ -7,19 +7,19 @@ import com.intive.patronage.toz.exception.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.context.request.WebRequest;
-import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @ControllerAdvice
-public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
+public class ControllerExceptionHandler {
 
     private MessageSource messageSource;
 
@@ -28,11 +28,28 @@ public class ControllerExceptionHandler extends ResponseEntityExceptionHandler {
         this.messageSource = messageSource;
     }
 
-    @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
-        String message = messageSource.getMessage("validationError", null, LocaleContextHolder.getLocale());
-        ValidationErrorResponse response = new ValidationErrorResponse(HttpStatus.BAD_REQUEST.value(), message, ex);
-        return handleExceptionInternal(ex, response, headers, status, request);
+//    @Override
+//    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+//        String message = messageSource.getMessage("validationError", null, LocaleContextHolder.getLocale());
+//        ValidationErrorResponse response = new ValidationErrorResponse(HttpStatus.BAD_REQUEST.value(), message, ex);
+//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(response);
+//        return handleExceptionInternal(ex, response, headers, status, request);
+//    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(value = HttpStatus.BAD_REQUEST)
+    @ResponseBody
+    public ValidationErrorResponse handleValidationException(MethodArgumentNotValidException e) {
+        String message = messageSource.getMessage("validationError",
+                null,
+                LocaleContextHolder.getLocale());
+        List<ValidationErrorResponse.SingleFieldError> errors = new ArrayList<>();
+        for (FieldError error : e.getBindingResult().getFieldErrors()) {
+            errors.add(new ValidationErrorResponse.SingleFieldError(error.getField(),
+                    error.getDefaultMessage(),
+                    error.getRejectedValue().toString()));
+        }
+        return new ValidationErrorResponse(HttpStatus.BAD_REQUEST.value(), message, errors);
     }
 
     @ExceptionHandler(AlreadyExistsException.class)
