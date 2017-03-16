90e23a8ba1a46cb8e0245b63a26c30c3ef7aa78c
Jakub <jakub.izbicki1@gmail.com>
2017-03-16 17:29:35 +0100

    adds additional tests

--- src/test/java/com/intive/patronage/toz/controller/OrganizationInfoControllerTest.java
+++ src/test/java/com/intive/patronage/toz/controller/OrganizationInfoControllerTest.java
@@ -10,11 +10,19 @@ import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.method.HandlerMethod;
+import org.springframework.web.method.annotation.ExceptionHandlerMethodResolver;
+import org.springframework.web.servlet.mvc.method.annotation.ExceptionHandlerExceptionResolver;
+import org.springframework.web.servlet.mvc.method.annotation.ServletInvocableHandlerMethod;
 
+import java.lang.reflect.Method;
 import java.nio.charset.Charset;
 
 import static org.mockito.Matchers.any;
@@ -30,6 +38,7 @@ public class OrganizationInfoControllerTest {
     private final static String PATH = "/organization/info";
     private final static String ORG_NAME = "Org";
     private final static String ACCOUNT = "63102047950000940201035419";
+    private final static String INVALID_ACCOUNT = "631-02047950000940201035419";
 
     private final MediaType contentType = new MediaType(MediaType.APPLICATION_JSON.getType(),
             MediaType.APPLICATION_JSON.getSubtype(),
@@ -40,17 +49,43 @@ public class OrganizationInfoControllerTest {
     @SuppressWarnings("SpringJavaAutowiredMembersInspection")
     @Autowired
     private ObjectMapper objectMapper;
+    @Autowired
+    private MessageSource messageSource;
 
     private MockMvc mvc;
+    private MockMvc mvcWithCustomHandlers;
     private OrganizationInfoView infoView;
+    private OrganizationInfoView invalidInfoView;
+
+    private ExceptionHandlerExceptionResolver createExceptionResolver() {
+        ExceptionHandlerExceptionResolver exceptionResolver = new ExceptionHandlerExceptionResolver() {
+            @Override
+            protected ServletInvocableHandlerMethod getExceptionHandlerMethod(
+                    HandlerMethod handlerMethod, Exception exception) {
+                Method method = new ExceptionHandlerMethodResolver(
+                        ControllerExceptionHandler.class).resolveMethod(exception);
+                return new ServletInvocableHandlerMethod(
+                        new ControllerExceptionHandler(messageSource), method);
+            }
+        };
+        exceptionResolver.getMessageConverters().add(new MappingJackson2HttpMessageConverter());
+        exceptionResolver.afterPropertiesSet();
+        return exceptionResolver;
+    }
 
     @Before
     public void setUp() throws Exception {
         mvc = MockMvcBuilders.standaloneSetup(new OrganizationInfoController(infoService)).build();
+        mvcWithCustomHandlers = MockMvcBuilders.standaloneSetup(new OrganizationInfoController(infoService))
+                .setHandlerExceptionResolvers(createExceptionResolver()).build();
 
         BankAccountView bankAccountView = new BankAccountView.Builder(ACCOUNT)
                 .build();
         infoView = new OrganizationInfoView.Builder(ORG_NAME, bankAccountView)
+                .build();
+        BankAccountView invalidBankAccountView = new BankAccountView.Builder(INVALID_ACCOUNT)
+                .build();
+        invalidInfoView = new OrganizationInfoView.Builder(ORG_NAME, invalidBankAccountView)
                 .build();
     }
 
@@ -123,5 +158,31 @@ public class OrganizationInfoControllerTest {
                 .andExpect(status().isBadRequest());
 
         verifyZeroInteractions(infoService);
+    }
+
+    @Test()
+    public void shouldReturnValidationErrorWhenInvalidFieldsInBodyInPutRequest() throws Exception {
+        final String validationErrorMessage = messageSource.getMessage("validationError",
+                null,
+                LocaleContextHolder.getLocale());
+        mvcWithCustomHandlers.perform(put(PATH)
+                .contentType(contentType)
+                .content(objectMapper.writeValueAsString(invalidInfoView)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("message").value(validationErrorMessage));
+    }
+
+    @Test()
+    public void shouldReturnValidationErrorWhenInvalidFieldsInBodyInPostRequest() throws Exception {
+        final String validationErrorMessage = messageSource.getMessage("validationError",
+                null,
+                LocaleContextHolder.getLocale());
+        mvcWithCustomHandlers.perform(post(PATH)
+                .contentType(contentType)
+                .content(objectMapper.writeValueAsString(invalidInfoView)))
+                .andExpect(status().isBadRequest())
+                .andExpect(content().contentType(contentType))
+                .andExpect(jsonPath("message").value(validationErrorMessage));
     }
 }
