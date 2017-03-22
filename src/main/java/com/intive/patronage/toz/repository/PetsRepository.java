2aaa65d8caada930443094f486e15e8394316311
Jakub <jakub.izbicki1@gmail.com>
2017-03-22 19:39:22 +0100

    changes resource for pets list to filter the result

--- src/main/java/com/intive/patronage/toz/repository/PetsRepository.java
+++ src/main/java/com/intive/patronage/toz/repository/PetsRepository.java
@@ -2,5 +2,8 @@ package com.intive.patronage.toz.repository;
 
 import com.intive.patronage.toz.model.db.Pet;
 
+import java.util.List;
+
 public interface PetsRepository extends IdentifiableRepository<Pet> {
+    List<Pet> findByNameNotNullAndTypeNotNullAndSexNotNull();
 }
