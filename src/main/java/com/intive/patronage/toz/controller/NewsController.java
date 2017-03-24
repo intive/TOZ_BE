b4100c56884a5677c7ebe1c459ac6f0fa77704ad
MateuszSz93 <spens16@gmail.com>
2017-03-24 22:39:57 +0100

    Added swagger documentation.

--- src/main/java/com/intive/patronage/toz/controller/NewsController.java
+++ src/main/java/com/intive/patronage/toz/controller/NewsController.java
@@ -1,7 +1,10 @@
 package com.intive.patronage.toz.controller;
 
+import com.intive.patronage.toz.error.ErrorResponse;
+import com.intive.patronage.toz.error.ValidationErrorResponse;
 import com.intive.patronage.toz.model.view.NewsView;
 import com.intive.patronage.toz.service.NewsService;
+import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +16,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.UUID;
 
+@Api(description = "News operations.")
 @RestController
 @RequestMapping(value = "/news", produces = MediaType.APPLICATION_JSON_VALUE)
 class NewsController {
@@ -23,8 +27,12 @@ class NewsController {
         this.newsService = newsService;
     }
 
+    @ApiOperation(value = "Get all news.", responseContainer = "List")
+    @ApiResponses(value = {
+            @ApiResponse(code = 422, message = "Unprocessable entity.", response = ErrorResponse.class)
+    })
     @GetMapping
-    public List<NewsView> getAllNews(@RequestParam(value = "type", required = false) String type,
+    public List<NewsView> getAllNews(@ApiParam(allowableValues = "ARCHIVED, RELEASED, UNRELEASED") @RequestParam(value = "type", required = false) String type,
                                      @RequestParam(value = "shortened", required = false, defaultValue = "false")
                                              Boolean shortened) {
         if (type != null) {
@@ -33,11 +41,19 @@ class NewsController {
         return newsService.findAllNews(shortened);
     }
 
+    @ApiOperation(value = "Get news by id.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 404, message = "News not found.", response = ErrorResponse.class),
+    })
     @GetMapping(value = "/{id}")
     public NewsView getNewsById(@PathVariable UUID id) {
         return newsService.findById(id);
     }
 
+    @ApiOperation(value = "Create news.", response = NewsView.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 400, message = "Bad request.", response = ValidationErrorResponse.class)
+    })
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<NewsView> createNews(@Valid @RequestBody NewsView newsView) {
         NewsView createdNews = newsService.createNews(newsView);
@@ -47,12 +63,21 @@ class NewsController {
                 .body(createdNews);
     }
 
+    @ApiOperation("Delete news.")
+    @ApiResponses(value = {
+            @ApiResponse(code = 404, message = "News not found.", response = ErrorResponse.class)
+    })
     @DeleteMapping(value = "/{id}")
     public ResponseEntity<NewsView> deleteNewsById(@PathVariable UUID id) {
         newsService.deleteNews(id);
         return ResponseEntity.ok().build();
     }
 
+    @ApiOperation(value = "Update news.", response = NewsView.class)
+    @ApiResponses(value = {
+            @ApiResponse(code = 404, message = "News not found.", response = ErrorResponse.class),
+            @ApiResponse(code = 400, message = "Bad request.", response = ValidationErrorResponse.class)
+    })
     @PutMapping(value = "/{id}", consumes = MediaType.APPLICATION_JSON_VALUE)
     public NewsView updateNews(@PathVariable UUID id, @Valid @RequestBody NewsView newsView) {
         return newsService.updateNews(id, newsView);
--- src/main/java/com/intive/patronage/toz/model/view/NewsView.java
+++ src/main/java/com/intive/patronage/toz/model/view/NewsView.java
@@ -5,32 +5,42 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import com.intive.patronage.toz.model.db.IdentifiableView;
 import com.intive.patronage.toz.model.db.News;
 import com.intive.patronage.toz.model.validator.EnumValidate;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import org.hibernate.validator.constraints.NotEmpty;
 
 import javax.validation.constraints.NotNull;
 
+@ApiModel(value = "News")
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 public class NewsView extends IdentifiableView {
+    @ApiModelProperty(example = "Do oddziału TOZ trafił nowy pies!", position = 1, required = true)
     @NotNull
     @NotEmpty
     private String title;
 
+    @ApiModelProperty(example = "Dzisiaj do naszej placówki w Szczecinie trafił nowy pies. Nazywa się Reksio i jest dwuletnim jamnikiem. Został znaleziony w pobliżu ogrodów działkowych.", position = 2, required = true)
     @NotNull
     @NotEmpty
     private String contents;
 
+    @ApiModelProperty(example = "RELEASED", allowableValues = "ARCHIVED, RELEASED, UNRELEASED", position = 3, required = true)
     @NotNull
     @EnumValidate(enumClass = News.Type.class)
     private String type;
 
+    @ApiModelProperty(example = "storage/a9/2c/a92ccd6a-f51c-4ff0-8645-02adff409051.jpg", position = 4)
     private String photoUrl;
 
+    @ApiModelProperty(example = "1222333444555", position = 5)
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long created;
 
+    @ApiModelProperty(example = "1222333444555", position = 6)
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
     private Long lastModified;
 
+    @ApiModelProperty(example = "1222333444555", position = 7)
     @JsonProperty(access = JsonProperty.Access.READ_WRITE)
     private Long published;
 
