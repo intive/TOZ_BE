98345b017e29b393979cdb47f3f8dd2d24711b6d
Łukasz Piskadło <lukasz.piskadlo@gmail.com>
2017-03-25 18:20:57 +0100

    Various fixes

--- src/main/java/com/intive/patronage/toz/schedule/DayConfig.java
+++ src/main/java/com/intive/patronage/toz/schedule/DayConfig.java
@@ -4,23 +4,25 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.time.DayOfWeek;
 import java.util.List;
 
 @ApiModel("Day configuration")
 @Getter
-@Setter
-@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 class DayConfig {
 
     @ApiModelProperty(value = "Name of day")
-    private DayOfWeek dayOfWeek;
+    private final DayOfWeek dayOfWeek;
+
     @ApiModelProperty(value = "List of periods in a day")
-    private List<Period> periods;
+    private final List<Period> periods;
+
+    public DayConfig(DayOfWeek dayOfWeek, List<Period> periods) {
+        this.dayOfWeek = dayOfWeek;
+        this.periods = periods;
+    }
 
     @ApiModelProperty(value = "Number of periods in a day")
     public int getNumberOfPeriods() {
--- src/main/java/com/intive/patronage/toz/schedule/ScheduleView.java
+++ src/main/java/com/intive/patronage/toz/schedule/ScheduleView.java
@@ -3,17 +3,18 @@ package com.intive.patronage.toz.schedule;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModel;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 import java.util.List;
 
 @ApiModel("Schedule")
 @Getter
-@Setter
-@NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
 class ScheduleView {
-    private List<ReservationView> reservations;
-    private List<DayConfig> configs;
+    private final List<ReservationView> reservations;
+    private final List<DayConfig> configs;
+
+    ScheduleView(List<ReservationView> reservations, List<DayConfig> configs) {
+        this.reservations = reservations;
+        this.configs = configs;
+    }
 }
