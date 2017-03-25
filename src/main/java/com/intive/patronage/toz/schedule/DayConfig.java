f68f9561078c58d5c39f39d7cbf6130ea2369019
Łukasz Piskadło <lukasz.piskadlo@gmail.com>
2017-03-25 16:51:21 +0100

    Add separate configs for each day

--- src/main/java/com/intive/patronage/toz/schedule/DayConfig.java
+++ src/main/java/com/intive/patronage/toz/schedule/DayConfig.java
@@ -2,22 +2,28 @@ package com.intive.patronage.toz.schedule;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.DayOfWeek;
 import java.util.List;
 
-@ApiModel("Period configuration")
+@ApiModel("Day configuration")
 @Getter
 @Setter
 @NoArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-class PeriodConfig {
+class DayConfig {
 
+    @ApiModelProperty(value = "Name of day")
+    private DayOfWeek dayOfWeek;
+    @ApiModelProperty(value = "List of periods in a day")
     private List<Period> periods;
 
-    public int getPeriodAmount() {
+    @ApiModelProperty(value = "Number of periods in a day")
+    public int getNumberOfPeriods() {
         return periods.size();
     }
 }
