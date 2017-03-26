8bb77a6303a3c151ef89e3deeaf46554f0bdf13e
MateuszSz93 <spens16@gmail.com>
2017-03-26 15:25:30 +0200

    Added private constructor for ApiUrl.

--- src/main/java/com/intive/patronage/toz/model/constant/ApiUrl.java
+++ src/main/java/com/intive/patronage/toz/model/constant/ApiUrl.java
@@ -4,4 +4,7 @@ public final class ApiUrl {
     public final static String PET_PATH = "/pets";
     public final static String NEWS_PATH = "/news";
     public final static String ORGANIZATION_INFO_PATH = "/organization/info";
+
+    private ApiUrl() {
+    }
 }
