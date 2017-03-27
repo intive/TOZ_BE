673bf38845ff28c876fa75871d367e28451b87b5
MateuszSz93 <spens16@gmail.com>
2017-03-27 10:36:17 +0200

    Added break in NewsService to be more readable on mobile.

--- src/main/java/com/intive/patronage/toz/service/NewsService.java
+++ src/main/java/com/intive/patronage/toz/service/NewsService.java
@@ -72,7 +72,8 @@ public class NewsService {
     private List<News> createShortenedNewsContents(Boolean shortened, List<News> newsList) {
         if (shortened) {
             for (News news : newsList) {
-                news.setContents(new StringFormatter().trimToLengthPreserveWord(news.getContents(), NEWS_DESCRIPTION_LENGTH));
+                news.setContents(new StringFormatter().
+                        trimToLengthPreserveWord(news.getContents(), NEWS_DESCRIPTION_LENGTH));
             }
         }
         return newsList;
