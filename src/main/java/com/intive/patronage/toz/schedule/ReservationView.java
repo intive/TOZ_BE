37e1225634f621577e21d13fe7763333cb6fac3c
Jakub <jakub.izbicki1@gmail.com>
2017-03-28 12:45:03 +0200

    updates ReservationView, fixes problems with dates serialization

--- src/main/java/com/intive/patronage/toz/schedule/ReservationView.java
+++ src/main/java/com/intive/patronage/toz/schedule/ReservationView.java
@@ -1,69 +1,127 @@
 package com.intive.patronage.toz.schedule;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
-import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import com.intive.patronage.toz.model.db.IdentifiableView;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
-import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.util.UUID;
 
-@ApiModel("Reservation")
-
-@Getter
-@Setter
-@Builder
-@NoArgsConstructor
-@AllArgsConstructor
+@ApiModel(value = "Reservation")
 @JsonInclude(JsonInclude.Include.NON_EMPTY)
-@JsonDeserialize(builder = ReservationView.ReservationViewBuilder.class)
 class ReservationView extends IdentifiableView {
 
-    @ApiModelProperty(value = "Date in UTC", required = true)
+    private final String LOCAL_DATE_PATTERN = "yyyy-MM-dd";
+    private final String LOCAL_TIME_PATTERN = "HH:mm";
+
+    @ApiModelProperty(value = "Date in UTC", example = "2017-10-20", required = true, position = 1)
     @NotNull
     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE)
+    @JsonFormat(pattern = LOCAL_DATE_PATTERN)
     private LocalDate date;
 
-    @ApiModelProperty(value = "Start time in UTC", required = true, example = "13:59")
+    @ApiModelProperty(value = "Start time in UTC", required = true, example = "13:59", position = 2)
     @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
     @NotNull
+    @JsonFormat(pattern = LOCAL_TIME_PATTERN)
     private LocalTime startTime;
 
-    @ApiModelProperty(value = "End time in UTC", required = true, example = "17:59")
+    @ApiModelProperty(value = "End time in UTC", required = true, example = "17:59", position = 3)
     @DateTimeFormat(iso = DateTimeFormat.ISO.TIME)
     @NotNull
+    @JsonFormat(pattern = LOCAL_TIME_PATTERN)
     private LocalTime endTime;
 
-    @ApiModelProperty(value = "Owner ID", example = "c5296892-347f-4b2e-b1c6-6faff971f767", required = true)
+    @ApiModelProperty(value = "Owner ID", example = "c5296892-347f-4b2e-b1c6-6faff971f767", required = true, position = 4)
     @Valid
     @NotNull
     private UUID ownerId;
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    @ApiModelProperty(readOnly = true)
-    private LocalDateTime created;
+    @ApiModelProperty(example = "1490134074968", position = 5)
+    private Long created;
 
     @JsonProperty(access = JsonProperty.Access.READ_ONLY)
-    @ApiModelProperty(readOnly = true)
-    private LocalDateTime lastModified;
+    @ApiModelProperty(example = "1490134074968", position = 6)
+    private Long lastModified;
 
-    @ApiModelProperty(value = "Modification message")
+    @ApiModelProperty(value = "Modification message", position = 7)
     private String modificationMessage;
 
-    @ApiModelProperty(value = "Modification author ID", example = "c5296892-347f-4b2e-b1c6-6faff971f767")
+    @ApiModelProperty(value = "Modification author ID", example = "c5296892-347f-4b2e-b1c6-6faff971f767", position = 8)
     private UUID modificationAuthorId;
 
-    @JsonPOJOBuilder(withPrefix = "")
-    public static final class ReservationViewBuilder {
+    public ReservationView() {
     }
 
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public LocalTime getStartTime() {
+        return startTime;
+    }
+
+    public void setStartTime(LocalTime startTime) {
+        this.startTime = startTime;
+    }
+
+    public LocalTime getEndTime() {
+        return endTime;
+    }
+
+    public void setEndTime(LocalTime endTime) {
+        this.endTime = endTime;
+    }
+
+    public UUID getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(UUID ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public Long getCreated() {
+        return created;
+    }
+
+    public void setCreated(Long created) {
+        this.created = created;
+    }
+
+    public Long getLastModified() {
+        return lastModified;
+    }
+
+    public void setLastModified(Long lastModified) {
+        this.lastModified = lastModified;
+    }
+
+    public String getModificationMessage() {
+        return modificationMessage;
+    }
+
+    public void setModificationMessage(String modificationMessage) {
+        this.modificationMessage = modificationMessage;
+    }
+
+    public UUID getModificationAuthorId() {
+        return modificationAuthorId;
+    }
+
+    public void setModificationAuthorId(UUID modificationAuthorId) {
+        this.modificationAuthorId = modificationAuthorId;
+    }
 }
