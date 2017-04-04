1adfc16598fa8df9551d4d17c7957b4c8ff01352
Marek Macko <macko.marek93@gmail.com>
2017-04-04 21:31:38 +0200

    loombok for news

--- src/main/java/com/intive/patronage/toz/news/model/db/News.java
+++ src/main/java/com/intive/patronage/toz/news/model/db/News.java
@@ -1,6 +1,8 @@
 package com.intive.patronage.toz.news.model.db;
 
 import com.intive.patronage.toz.base.model.Identifiable;
+import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
@@ -8,7 +10,9 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import javax.persistence.*;
 import java.util.Date;
 
-@Entity // TODO loombok
+@Getter
+@Setter
+@Entity
 @EntityListeners(AuditingEntityListener.class)
 public class News extends Identifiable {
     private String title;
@@ -28,62 +32,6 @@ public class News extends Identifiable {
 
     @Temporal(TemporalType.TIMESTAMP)
     private Date published;
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContents() {
-        return contents;
-    }
-
-    public void setContents(String contents) {
-        this.contents = contents;
-    }
-
-    public Type getType() {
-        return type;
-    }
-
-    public void setType(Type type) {
-        this.type = type;
-    }
-
-    public String getPhotoUrl() {
-        return photoUrl;
-    }
-
-    public void setPhotoUrl(String photoUrl) {
-        this.photoUrl = photoUrl;
-    }
-
-    public Date getCreated() {
-        return created;
-    }
-
-    public void setCreated(Date created) {
-        this.created = created;
-    }
-
-    public Date getLastModified() {
-        return lastModified;
-    }
-
-    public void setLastModified(Date lastModified) {
-        this.lastModified = lastModified;
-    }
-
-    public Date getPublished() {
-        return published;
-    }
-
-    public void setPublished(Date published) {
-        this.published = published;
-    }
 
     public enum Type {
         RELEASED, UNRELEASED, ARCHIVED
