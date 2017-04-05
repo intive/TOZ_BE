d16d3193304169c4c352a8f977dd8266fc6a0046
Marek Macko <macko.marek93@gmail.com>
2017-04-05 18:56:50 +0200

    fix user service tests

--- src/test/java/com/intive/patronage/toz/users/UserServiceTest.java
+++ src/test/java/com/intive/patronage/toz/users/UserServiceTest.java
@@ -3,12 +3,14 @@ package com.intive.patronage.toz.users;
 
 import com.intive.patronage.toz.error.exception.NotFoundException;
 import com.intive.patronage.toz.users.model.db.User;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.util.Collections;
 import java.util.List;
@@ -18,23 +20,16 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.eq;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
-
+@RunWith(DataProviderRunner.class)
 public class UserServiceTest {
     private static final UUID EXPECTED_ID = UUID.randomUUID();
     private static final String EXPECTED_NAME = "Johny";
     private static final String EXPECTED_PASSWORD = "secret";
     private static final String EXPECTED_SURNAME = "Bravo";
     private static final User.Role EXPECTED_ROLE = User.Role.TOZ;
-
-    private User user;
-    private UUID userId;
 
     @Mock
     private UserRepository userRepository;
@@ -43,55 +38,61 @@ public class UserServiceTest {
     private UserService userService;
 
     @Before
-    public void setUp() throws Exception { // TODO: data provider
+    public void setUp() throws Exception {
         MockitoAnnotations.initMocks(this);
         userService = new UserService(userRepository);
-        user = new User();
+    }
+
+    @DataProvider
+    public static Object[] getProperUser() {
+        final User user = new User();
         user.setId(EXPECTED_ID);
         user.setName(EXPECTED_NAME);
         user.setPassword(EXPECTED_PASSWORD);
         user.setSurname(EXPECTED_SURNAME);
         user.setRole(EXPECTED_ROLE);
-        userId = user.getId();
+        return new User[]{user};
     }
 
     @Test
     public void findAllUsers() throws Exception {
         when(userRepository.findAll()).thenReturn(Collections.emptyList());
 
-        List<User> users = userService.findAll();
+        final List<User> users = userService.findAll();
         assertTrue(users.isEmpty());
     }
 
     @Test
-    public void findById() throws Exception {
-        when(userRepository.exists(userId)).thenReturn(true);
-        when(userRepository.findOne(userId)).thenReturn(user);
+    @UseDataProvider("getProperUser")
+    public void findById(final User user) throws Exception {
+        when(userRepository.exists(EXPECTED_ID)).thenReturn(true);
+        when(userRepository.findOne(EXPECTED_ID)).thenReturn(user);
 
-        User dbUser = userService.findOneById(userId);
+        final User dbUser = userService.findOneById(EXPECTED_ID);
         assertEquals(EXPECTED_NAME, dbUser.getName());
         assertEquals(EXPECTED_PASSWORD, dbUser.getPassword());
         assertEquals(EXPECTED_SURNAME, dbUser.getSurname());
         assertEquals(EXPECTED_ROLE, dbUser.getRole());
 
-        verify(userRepository, times(1)).exists(eq(userId));
-        verify(userRepository, times(1)).findOne(eq(userId));
+        verify(userRepository, times(1)).exists(eq(EXPECTED_ID));
+        verify(userRepository, times(1)).findOne(eq(EXPECTED_ID));
         verifyNoMoreInteractions(userRepository);
     }
 
     @Test(expected = NotFoundException.class)
     public void findByIdNotFoundException() throws Exception {
-        when(userRepository.exists(userId)).thenReturn(false);
-        userService.findOneById(userId);
+        when(userRepository.exists(EXPECTED_ID)).thenReturn(false);
+        userService.findOneById(EXPECTED_ID);
 
-        verify(userRepository, times(1)).exists(eq(userId));
+        verify(userRepository, times(1)).exists(eq(EXPECTED_ID));
         verifyNoMoreInteractions(userRepository);
     }
 
     @Test
-    public void createUser() throws Exception {
+    @UseDataProvider("getProperUser")
+    public void createUser(final User user) throws Exception {
         when(userRepository.save(any(User.class))).thenReturn(user);
-        User createdUser = userService.create(user);
+        final User createdUser = userService.create(user);
         assertEquals(EXPECTED_NAME, createdUser.getName());
         assertEquals(EXPECTED_PASSWORD, createdUser.getPassword());
         assertEquals(EXPECTED_SURNAME, createdUser.getSurname());
@@ -102,28 +103,29 @@ public class UserServiceTest {
 
     @Test
     public void deleteUser() throws Exception {
-        when(userRepository.exists(userId)).thenReturn(true);
-        userService.delete(userId);
+        when(userRepository.exists(EXPECTED_ID)).thenReturn(true);
+        userService.delete(EXPECTED_ID);
 
-        verify(userRepository, times(1)).exists(eq(userId));
-        verify(userRepository, times(1)).delete(eq(userId));
+        verify(userRepository, times(1)).exists(eq(EXPECTED_ID));
+        verify(userRepository, times(1)).delete(eq(EXPECTED_ID));
         verifyNoMoreInteractions(userRepository);
     }
 
     @Test(expected = NotFoundException.class)
     public void deleteUserNotFoundException() throws Exception {
-        when(userRepository.exists(userId)).thenReturn(false);
-        userService.delete(userId);
+        when(userRepository.exists(EXPECTED_ID)).thenReturn(false);
+        userService.delete(EXPECTED_ID);
 
-        verify(userRepository, times(1)).exists(eq(userId));
+        verify(userRepository, times(1)).exists(eq(EXPECTED_ID));
         verifyNoMoreInteractions(userRepository);
     }
 
     @Test
-    public void updateUser() throws Exception {
-        when(userRepository.exists(userId)).thenReturn(true);
+    @UseDataProvider("getProperUser")
+    public void updateUser(final User user) throws Exception {
+        when(userRepository.exists(EXPECTED_ID)).thenReturn(true);
         when(userRepository.save(any(User.class))).thenReturn(user);
-        User savedUser = userService.update(userId, user);
+        final User savedUser = userService.update(EXPECTED_ID, user);
 
         assertEquals(EXPECTED_NAME, savedUser.getName());
         assertEquals(EXPECTED_PASSWORD, savedUser.getPassword());
@@ -132,11 +134,12 @@ public class UserServiceTest {
     }
 
     @Test(expected = NotFoundException.class)
-    public void updateUserNotFoundException() throws Exception {
-        when(userRepository.exists(userId)).thenReturn(false);
-        userService.update(userId, user);
+    @UseDataProvider("getProperUser")
+    public void updateUserNotFoundException(final User user) throws Exception {
+        when(userRepository.exists(EXPECTED_ID)).thenReturn(false);
+        userService.update(EXPECTED_ID, user);
 
-        verify(userRepository, times(1)).exists(eq(userId));
+        verify(userRepository, times(1)).exists(eq(EXPECTED_ID));
         verifyNoMoreInteractions(userRepository);
     }
 
