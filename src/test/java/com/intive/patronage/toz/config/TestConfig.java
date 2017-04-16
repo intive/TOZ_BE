cd1f041d9734ac0368c5295968b90f237932ed3d
Łukasz Piskadło <lukasz.piskadlo@gmail.com>
2017-04-16 23:06:51 +0200

    Add property for ScheduleParserTest

--- src/test/java/com/intive/patronage/toz/config/TestConfig.java
+++
@@ -1,9 +0,0 @@
-package com.intive.patronage.toz.config;
-
-import org.springframework.boot.test.context.TestConfiguration;
-import org.springframework.test.context.TestPropertySource;
-
-@TestConfiguration
-@TestPropertySource(properties = "jwt.secret-base64=c2VjcmV0")
-public class TestConfig {
-}
--- src/test/java/com/intive/patronage/toz/schedule/ScheduleParserTest.java
+++ src/test/java/com/intive/patronage/toz/schedule/ScheduleParserTest.java
@@ -9,6 +9,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.AbstractJUnit4SpringContextTests;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
@@ -28,6 +29,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = Application.class)
 @WebAppConfiguration
+@TestPropertySource(properties = "jwt.secret-base64=c2VjcmV0")
 public class ScheduleParserTest extends AbstractJUnit4SpringContextTests {
 
     private static final String DASH = "-";
