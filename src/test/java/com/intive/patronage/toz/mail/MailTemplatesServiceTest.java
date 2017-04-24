775ac487ce231b60b35801819a823b862469a702
macieg-b <bartlomiejczyk.maciej@gmail.com>
2017-04-24 16:54:27 +0200

    Fix error connected with database

--- src/test/java/com/intive/patronage/toz/mail/MailTemplatesServiceTest.java
+++ src/test/java/com/intive/patronage/toz/mail/MailTemplatesServiceTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.IOException;
@@ -12,6 +13,7 @@ import static org.junit.Assert.assertTrue;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
 public class MailTemplatesServiceTest {
 
     @Autowired
