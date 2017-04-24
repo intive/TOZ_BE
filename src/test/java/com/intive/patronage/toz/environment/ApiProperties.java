7347e7fe534bb879b3526b5ad2c4f63dbbda94c8
Marek Macko <macko.marek93@gmail.com>
2017-04-24 20:33:52 +0200

    ApiProperties change to final

--- src/test/java/com/intive/patronage/toz/environment/ApiProperties.java
+++ src/test/java/com/intive/patronage/toz/environment/ApiProperties.java
@@ -1,6 +1,6 @@
 package com.intive.patronage.toz.environment;
 
-public class ApiProperties {
+public final class ApiProperties {
     public static final String JWT_SECRET_BASE64 = "jwt.secret-base64=c2VjcmV0";
     public static final String SUPER_ADMIN_PASSWORD = "super-admin.password=secretPassword";
 
