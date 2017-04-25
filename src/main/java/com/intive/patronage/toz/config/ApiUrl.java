c78cba603860f1bc5c4b40f968ff3b8745489380
macieg-b <bartlomiejczyk.maciej@gmail.com>
2017-04-25 13:07:47 +0200

    Tozpatron 168 mail body template (#35)
    * Add handlebars dependencies and methods to create necessary handlebars objects.
    * Add unit test and model for registration template.

--- src/main/java/com/intive/patronage/toz/config/ApiUrl.java
+++ src/main/java/com/intive/patronage/toz/config/ApiUrl.java
@@ -6,6 +6,7 @@ public final class ApiUrl {
     public final static String ADMIN_USERS_PATH = "/admin/users";
     public final static String ORGANIZATION_INFO_PATH = "/organization/info";
     public final static String TOKENS_PATH = "/tokens";
+    public final static String ACTIVATION_PATH = "/activate";
     public final static String ACQUIRE_TOKEN_PATH = TOKENS_PATH + "/acquire";
 
     private ApiUrl() {
