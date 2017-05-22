b5844407d87396fea198c2983d143cbd0f63124a
Jakub Izbicki <jakub.izbicki1@gmail.com>
2017-05-22 12:28:15 +0200

    deletes redundant code

--- src/main/java/com/intive/patronage/toz/util/validation/NotPastValidator.java
+++ src/main/java/com/intive/patronage/toz/util/validation/NotPastValidator.java
@@ -1,18 +1,10 @@
 package com.intive.patronage.toz.util.validation;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.i18n.LocaleContextHolder;
-
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.time.LocalDate;
 
 public class NotPastValidator implements ConstraintValidator<NotPast, LocalDate> {
-
-    private static final String NULL_MESSAGE = "may not be null";
 
     @Override
     public void initialize(NotPast constraintAnnotation) {
